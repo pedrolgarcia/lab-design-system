fb7307c87678ad1d9c094dded2a75955162b1f9f
Pedro Lopes <pedrolopesgrc@gmail.com>
2022-10-12 15:57:21 -0300

    feat: import tokens

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import "./styles/global.css"
 
 export function App() {
   return (
-    <div className="bg-zinc-800 w-full h-full flex">
-      <h1 className="text-green-400">Hello World!</h1>
+    <div>
+      <h1 className="text-violet-600 font-bold">Hello World!</h1>
     </div>
   )
 }
